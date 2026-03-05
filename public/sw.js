fa1ac0be90e0a8e736e7e258521878380a766266
AndreaGpeQZ <andi.agq@gmail.com>
2026-03-04 17:25:07 -0800

    xdd

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = "pwa-cache-v2";
-const APP_SHELL = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
+const APP_SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
